9c639fec7523ba3d041dc55c180e3e2bebeda27b
Arvin.zhao <arvin.zhao@Arvinzhaos-MBP.lan>
2016-05-02 10:39:36 +0800

    d

--- src/com/test/Source.java
+++ src/com/test/Source.java
@@ -5,10 +5,6 @@ package com.test;
  */
 public class Source {
 	public static void main(String[] args) {
-		System.ou.println("hha");
-		System.ou.println("haha");
-
-		System.ou.println("hahaha");
-		System.ou.println("hahahaBufferCar");
+		System.ou.println("shoppingcsar");
 	}
 }
