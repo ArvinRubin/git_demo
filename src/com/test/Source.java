bf4fd521d6683c4eaa98365d39c015674de1199c
Arvin.zhao <arvin.zhao@coupang.com>
2017-08-03 22:02:39 +0800

    commit-1

--- src/com/test/Source.java
+++ src/com/test/Source.java
@@ -4,7 +4,8 @@ package com.test;
  * Created by arvin.zhao on 5/2/16.
  */
 public class Source {
+	
 	public static void main(String[] args) {
-		System.ou.println("shoppingcsar");
+		System.out.println("shoppingcsar");
 	}
 }
